7af3124cf76c23431d16b53cb62303195e7786ba
swapnildubey29 <swapnildubey3636@gmail.com>
2025-01-29 12:36:25 +0530

    Flashcard Question Api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import FlashcardList from './FlashcardList';
 import './app.css';
+import axios from 'axios';
+
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+  
+  useEffect(() => {
+    axios
+    .get('https://opentdb.com/api.php?amount=15&category=9&difficulty=medium&type=multiple')
+    .then(res => {
+       setFlashcards(res.data.results.map((questionItem, index) => {
+        const answer = decodeString(questionItem.correct_answer)
+        const options = [
+          ...questionItem.incorrect_answers.map(a => decodeString(a)),
+           answer
+          ]  
+        return{
+            id: `${index}- ${Date.now()}`,
+            question: questionItem.question,
+            answer: answer,
+            options: options.sort(() => Math.random() - .5)
+          }
+       }))
+    })
+  }, [])
+
+  function decodeString(str){
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+      return textArea.value
+  }
+  
+  
   return (
+    <div className='container'>
      <FlashcardList flashcards={flashcards} />
+     </div>
   );
 }
 
